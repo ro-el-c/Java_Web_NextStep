7e625c8281fce3bb3ff7ec994a0343490876776e
ro-el-c <chung3286@naver.com>
2023-11-30 02:26:31 +0900

    refactor: 단일 책임 부여를 위한 메서드 분리
    isBlank(), toIntegerList(), toInteger(), validateNumber()

--- src/main/java/string_calculator/StringCalculator.java
+++ src/main/java/string_calculator/StringCalculator.java
@@ -1,48 +1,67 @@
 package string_calculator;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    private final int MIN_NUM_OF_DIGIT = 1;
 
     public int add(String inputText) {
         List<Integer> numbers;
 
-        if (inputText == null || inputText.isBlank()) {
+        if (isBlank(inputText)) {
             return 0;
         }
 
-        String[] numberStrings;
-
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(inputText);
-        if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            numberStrings = matcher.group(2).split(customDelimiter);
-        } else { //TODO: else 제거
-            numberStrings = inputText.split(",|:");
-        }
-
+        String[] numberStrings = splitText(inputText);
         numbers = toIntegerList(numberStrings);
-        if (numbers.stream().anyMatch(number -> number < 0)) {
-            throw new RuntimeException("음수가 존재합니다.");
-        }
-
-        if (numbers.size() == MIN_NUM_OF_DIGIT) {
-            return Integer.parseInt(inputText);
-        }
-
         int result = sum(numbers);
 
         return result;
+
+        //축약: return sum(toIntegerList(splitText(inputText)));
+    }
+
+    private static boolean isBlank(String inputText) {
+        return inputText == null || inputText.isBlank();
+    }
+
+    private static String[] splitText(String inputText) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(inputText);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
+        return inputText.split(",|:");
     }
 
     private static List<Integer> toIntegerList(String[] numberStrings) {
-        return Arrays.stream(numberStrings)
-                .map(Integer::parseInt)
-                .toList();
+        List<Integer> numbers = new ArrayList<>();
+
+
+        for (String numberText : numberStrings) {
+            int number = toInteger(numberText);
+            validateNumber(number);
+            numbers.add(number);
+        }
+
+        return numbers;
+    }
+
+    private static int toInteger(String numberText) {
+        try {
+            return Integer.parseInt(numberText);
+        } catch (Exception e) {
+            throw new RuntimeException("숫자가 아닌 문자가 포함되어 있습니다.");
+        }
+    }
+
+    private static void validateNumber(Integer number) {
+        if (number < 0) {
+            throw new RuntimeException("음수가 존재합니다.");
+        }
     }
 
     private static int sum(List<Integer> numbers) {
