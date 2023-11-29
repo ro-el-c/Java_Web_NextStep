ee88acf956d3ee1754ebefa98b57a6a38f43ae77
ro-el-c <chung3286@naver.com>
2023-11-30 01:55:43 +0900

    refactor: split 로직 수정
    split_text_without_comma() 테스트 결과에 따른 수정

--- src/main/java/string_calculator/StringCalculator.java
+++ src/main/java/string_calculator/StringCalculator.java
@@ -6,6 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private final int MIN_NUM_OF_DIGIT = 1;
+
     public int add(String inputText) {
         List<Integer> numbers;
 
@@ -13,28 +15,23 @@ public class StringCalculator {
             return 0;
         }
 
-        try {
-            int number = Integer.parseInt(inputText);
-            if (number < 0) {
-                throw new RuntimeException("음수가 존재합니다.");
-            }
-            return number;
-        } catch (Exception e) {
-            String[] numberStrings;
+        String[] numberStrings;
 
-            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(inputText);
-            if (matcher.find()) {
-                String customDelimiter = matcher.group(1);
-                numberStrings = matcher.group(2).split(customDelimiter);
-            } else {
-                numberStrings = inputText.split(",|;");
-            }
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(inputText);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            numberStrings = matcher.group(2).split(customDelimiter);
+        } else { //TODO: else 제거
+            numberStrings = inputText.split(",|;");
+        }
 
-            numbers = Arrays.stream(numberStrings).map(Integer::parseInt).toList();
-            if (numbers.stream().anyMatch(number -> number < 0)) {
-                throw new RuntimeException("음수가 존재합니다.");
-            }
+        numbers = Arrays.stream(numberStrings).map(Integer::parseInt).toList();
+        if (numbers.stream().anyMatch(number -> number < 0)) {
+            throw new RuntimeException("음수가 존재합니다.");
+        }
 
+        if (numbers.size() == MIN_NUM_OF_DIGIT) {
+            return Integer.parseInt(inputText);
         }
 
         int result = 0;
